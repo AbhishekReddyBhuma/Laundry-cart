98ae08d0030436208ad6e53f907dc844a39f4fb7
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-02-21 19:29:13 +0530

    txt file added to show the mentor

--- server/models/orders.js
+++ server/models/orders.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 const ordersSchema = mongoose.Schema({
-
-})
+  productsName: {
+    type: Array,
+  },
+  serviceType: {
+    type: Object,
+  },
+  quantity: {
+    type: Array,
+  },
+  totalQuantity: {
+    type: Number,
+  },
+  finalAmount: {
+    type: Number,
+  },
+  pickUpCharge: {
+    type: Number,
+  },
+  totalAmount: { type: Number },
+  storeLoctaion: { type: String },
+  storeCite: { type: String },
+  storePhoneNumber: { type: Number },
+});
 
 module.exports = mongoose.model("Orders", ordersSchema);
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -2,17 +2,36 @@ const express = require("express");
 const router = express.Router();
 const orders = require("../models/orders");
 
-router.get("/all",async(req,res) => {
-    try{
-        const pastOrders = await orders.find();
-        res.status(200).json({
-            pastOrders
-        })
-    }catch(e){
-        res.status(400).json({
-            error:e.message
-        })
-    }
-})
+router.get("/all", async (req, res) => {
+  try {
+    const pastOrders = await orders.find();
+    res.status(200).json({
+      pastOrders,
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: e.message,
+    });
+  }
+});
+
+router.post("/create/order", async (req, res) => {
+  const { productsName, serviceType, quantity, finalAmount, pickUpCharge } =
+    req.body;
+  try {
+    const order = await orders.create({
+      productsName,
+      serviceType,
+      quantity,
+      finalAmount,
+      pickUpCharge,
+    });
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(400).json({
+      error: e.message,
+    });
+  }
+});
 
 module.exports = router;
