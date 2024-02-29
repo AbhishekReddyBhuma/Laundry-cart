f05399a6ebbeb19dc479da4fb5bb68800833d4a7
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-02-29 11:36:43 +0530

    Update Orders.jsx

--- laundry-cart/src/components/PastOrders/Orders.jsx
+++ laundry-cart/src/components/PastOrders/Orders.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { FiEye } from "react-icons/fi";
+import { FaSearchPlus } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
 import { contextProvider } from "../../Context/Context";
 import { format } from "date-fns";
+import "./orders.css";
+
 const Orders = () => {
-  const { PastOrders, FilterdPastOrder } = contextProvider();
+  const { PastOrders, FilterdPastOrder, getAllPastOrders } = contextProvider();
+  const location = useLocation();
+  console.log(location.pathname);
+
   // const [Porders, setPorders] = useState([]);
-  let fialPrice = 0;
+  useEffect(() => {
+    getAllPastOrders();
+  }, [location.pathname === "/orders"]);
+  let finalPrice = 0;
 
   console.log(PastOrders);
   let pOrders = [];
@@ -27,13 +38,21 @@ const Orders = () => {
   }
 
   console.log(...pOrders);
-  let formatedDte = format(new Date(pOrders[1][1]), "dd MMM yyyy, HH:mm");
-  console.log(formatedDte);
 
   return (
     <>
-      <div>
-        <table>
+      <div className="pastOrder">
+        <div className="inputHolder">
+          <span>orders 0</span>
+          <div className="btn">
+            <button>
+              <Link to="/products">create</Link>
+            </button>
+            <FaSearchPlus className="serchIcon" />
+            <input type="text" />
+          </div>
+        </div>
+        <table className="table">
           <thead>
             <tr>
               <th>Order Id</th>
@@ -44,36 +63,40 @@ const Orders = () => {
               <th>Total Items</th>
               <th>Price</th>
               <th>Status</th>
-              <th></th>
+
               <th>View</th>
             </tr>
           </thead>
           <tbody>
-            {pOrders.map((order, index) => (
-              <tr key={index}>
-                <td>{order[6]} </td>
-                <td>{format(new Date(order[1]), "dd MMM yyyy, HH:mm")} </td>
-                <td>{order[2]}</td>
-                <td>{order[3]}</td>
-                <td>{order[4]}</td>
-                <td>{order[7]}</td>
-                <td>{order[0][4]}</td>
-                <td>
-                  {order[0].map((price, index) => {
-                    fialPrice += price[4];
-                    if (order[0].length - 1 === index) {
-                      return fialPrice;
-                    }
-                  })}
-                </td>
-                <td>{order[8]}</td>
-                <td>
-                  <button onClick={() => FilterdPastOrder(order[6])}>
+            {pOrders.map((order, index) => {
+              finalPrice = 0;
+              return (
+                <tr key={index}>
+                  <td>{order[6]} </td>
+                  <td>{format(new Date(order[1]), "dd MMM yyyy, HH:mm")} </td>
+                  <td>{order[2]}</td>
+                  <td>{order[3]}</td>
+                  <td>{order[4]}</td>
+                  <td>{order[7]}</td>
+
+                  <td>
+                    {order[0].map((price, index) => {
+                      finalPrice += price[4];
+                      if (order[0].length - 1 === index) {
+                        return finalPrice;
+                      }
+                    })}
+                  </td>
+                  <td>{order[8]}</td>
+                  <td>
+                    <FiEye onClick={() => FilterdPastOrder(order[6])} />
+                    {/* <button >
                     view
-                  </button>
-                </td>
-              </tr>
-            ))}
+                  </button> */}
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
